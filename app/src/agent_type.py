ff18ca3fbee264fba21bd039a20f9468c9f3c781
federico.moreno1 <federico.moreno@kavak.com>
2023-06-23 16:47:04 -0300

    App

---
+++ app/src/agent_type.py
@@ -0,0 +1,50 @@
+"""
+## `DialogueAgent` class
+The `DialogueAgent` class is a simple wrapper around the `ChatOpenAI` model that stores the message history from the `dialogue_agent`'s point of view by simply concatenating the messages as strings.
+
+It exposes two methods:
+- `send()`: applies the chatmodel to the message history and returns the message string
+- `receive(name, message)`: adds the `message` spoken by `name` to message history
+
+"""
+from typing import List, Dict, Callable
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    HumanMessage,
+    SystemMessage,
+)
+
+class DialogueAgent:
+    def __init__(
+        self,
+        name: str,
+        system_message: SystemMessage,
+        model: ChatOpenAI,
+    ) -> None:
+        self.name = name
+        self.system_message = system_message
+        self.model = model
+        self.prefix = f"{self.name}: "
+        self.reset()
+
+    def reset(self):
+        self.message_history = ["Here is the conversation so far."]
+
+    def send(self) -> str:
+        """
+        Applies the chatmodel to the message history
+        and returns the message string
+        """
+        message = self.model(
+            [
+                self.system_message,
+                HumanMessage(content="\n".join(self.message_history + [self.prefix])),
+            ]
+        )
+        return message.content
+
+    def receive(self, name: str, message: str) -> None:
+        """
+        Concatenates {message} spoken by {name} into message history
+        """
+        self.message_history.append(f"{name}: {message}")
